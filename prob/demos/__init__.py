6f9e238fa5a135a86fbc22b375c5681803ace867
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-07-08 21:33:13 +1200

    Add scaled Gaussian

--- prob/demos/__init__.py
+++ prob/demos/__init__.py
@@ -1,3 +1,4 @@
 from .rv_sum_demo1 import *
+from .gauss_scaled_demo1 import *
 from .gauss_sum_demo1 import *
 from .gauss_demo1 import *
---
+++ prob/demos/gauss_scaled_demo1.py
@@ -0,0 +1,28 @@
+# M. P. Hayes UCECE
+import numpy as np
+from ipywidgets import interact, interactive, fixed, interact
+from .lib.signal_plot import signal_plot2
+from .lib.utils import gauss
+
+def gauss_scaled_demo1_plot(muX=0, sigmaX=1, a=1):
+
+    N = 401
+    x = np.linspace(-10, 10, N)
+
+    muY = a * muX
+    sigmaY = np.sqrt(abs(a)) * sigmaX
+    
+    fX = gauss(x, muX, sigmaX)
+    fY = gauss(x, muY, sigmaY)
+    
+    signal_plot2(x, fX, x, fY)
+
+def gauss_scaled_demo1():
+    interact(gauss_scaled_demo1_plot, muX=(-5, 5), sigmaX=(0.01, 5, 0.01),
+             a = (0.01, 5, 0.01),
+             continuous_update=False)
+    
+    
+
+    
+
