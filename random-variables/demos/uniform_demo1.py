a69279490d141572bc1a0af82935f0cf9666373a
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-07-16 20:07:05 +1200

    Fix uniform

--- random-variables/demos/uniform_demo1.py
+++ random-variables/demos/uniform_demo1.py
@@ -11,7 +11,13 @@ def uniform_demo1_plot(a=-1, b=1):
     x = np.linspace(-10, 10, N)
 
     fX = 1.0 * ((x >= a) & (x <= b)) / (abs(b - a) + 1e-12)
-    signal_plot(x, fX)
+    fig = signal_plot(x, fX)
+    mu_X = 0.5 * (a + b)
+    sigma_X = abs(b - a) / np.sqrt(12)
+    if b < a:
+        mu_X = 0
+        sigma_X = 0
+    fig.axes[0].set_title('$\mu_{X}$ = %.1f, $\sigma_{X}$ = %.1f' % (mu_X, sigma_X))
     show()    
 
 def uniform_demo1():
