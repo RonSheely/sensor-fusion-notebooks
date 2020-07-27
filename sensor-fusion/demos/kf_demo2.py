6cdbd07552b90c9d5382cacfb188c5704a9aec83
Michael Hayes <michael.hayes@canterbury.ac.nz>
2020-07-27 15:27:10 +1200

    Add show to kf_demo2

--- sensor-fusion/demos/kf_demo2.py
+++ sensor-fusion/demos/kf_demo2.py
@@ -5,9 +5,10 @@ from matplotlib.pyplot import subplots
 from .lib.utils import gauss
 
 steps = 10
+show_choices = ['Estimates', 'PDFs', 'Estimates and PDFs']
 
-def kf_demo2_plot(v=2.0, sigmaX0=0.1, sigmaV=0.4, sigmaW=0.4,
-                  seed=1, step=1):
+def kf_demo2_plot(show=show_choices[-1], v=2.0, sigmaX0=0.1, sigmaV=1,
+                  sigmaW=0.4, seed=1, step=1):
 
     np.random.seed(seed)
 
@@ -24,19 +25,27 @@ def kf_demo2_plot(v=2.0, sigmaX0=0.1, sigmaV=0.4, sigmaW=0.4,
     xrobot = 0
     Xinitialmean = 0
     Xinitialvar = sigmaX0**2
-    
-    fig, axes = subplots(2, figsize=(10, 6))
-    axes[0].set_xlim(0, steps)
-    axes[0].set_ylim(0, steps * B * v)    
-    axes[0].grid(True)    
-    axes[0].plot(0, xrobot, '-o', color='C0', label='actual')
-    axes[0].plot(0, 0, '+', color='C1', label='measured')
-    axes[0].plot(0, 0, 'x', color='C2', label='predicted')
-    axes[0].plot(0, Xinitialmean, '*', color='C3', label='estimated') 
-    axes[0].legend()
 
-    axes[1].grid(True)
-    
+    if show == show_choices[2]:
+        fig, axes = subplots(2, figsize=(10, 6))
+        estax, pdfax = axes
+    elif show == show_choices[1]:
+        fig, pdfax = subplots(1, figsize=(10, 6))
+        estax = None
+    else:
+        fig, estax = subplots(1, figsize=(10, 6))
+        pdfax = None
+        
+    if estax is not None:        
+            estax.set_xlim(0, steps)
+            estax.set_ylim(0, steps * B * v)    
+            estax.grid(True)    
+            estax.plot(0, xrobot, '-o', color='C0', label='actual')
+            estax.plot(0, 0, 'x', color='C1', label='prior')
+            estax.plot(0, 0, '+', color='C2', label='likelihood')
+            estax.plot(0, Xinitialmean, '*', color='C3', label='posterior') 
+            estax.legend()
+
     mx = (x < round(steps * B * v)) & (x > -2)
 
     for m in range(1, step + 1):
@@ -62,32 +71,36 @@ def kf_demo2_plot(v=2.0, sigmaX0=0.1, sigmaV=0.4, sigmaW=0.4,
         Xpostmean = K * Xinfermean + (1 - K) * Xpriormean
         Xpostvar = (Xpriorvar * Xinfervar) / (Xpriorvar + Xinfervar)
 
-        axes[0].plot(m, xrobot, 'o', color='C0')
-        axes[0].plot(m, Xinfermean, '+', color='C1')
-        axes[0].plot(m, Xpriormean, 'x', color='C2')                
-        axes[0].plot(m, Xpostmean, '*', color='C3')
-
+        if estax is not None:
+            estax.plot(m, xrobot, 'o', color='C0')
+            estax.plot(m, Xpriormean, 'x', color='C1')                
+            estax.plot(m, Xinfermean, '+', color='C2')
+            estax.plot(m, Xpostmean, '*', color='C3')
 
     fXinitial = gauss(x, Xinitialmean, np.sqrt(Xinitialvar))
     fXprior = gauss(x, Xpriormean, np.sqrt(Xpriorvar))
     fXinfer = gauss(x, Xinfermean, np.sqrt(Xinfervar))
     fXpost = gauss(x, Xpostmean, np.sqrt(Xpostvar))
 
-    axes[1].plot(x[mx], fXinitial[mx], ':', label='$X_{%d}$ initial' % (m - 1))        
-    axes[1].plot(x[mx], fXprior[mx], '--', label='$X_{%d}^{-}$ prior' % m)
-    axes[1].plot(x[mx], fXinfer[mx], '-.', label='$X_{%d}^{m}$ likelihood' % m)
-    axes[1].plot(x[mx], fXpost[mx], label='$X_{%d}^{+}$ posterior' % m)
+    if pdfax is not None:
+        pdfax.plot(x[mx], fXinitial[mx], ':', label='$X_{%d}$ initial' % (m - 1))        
+        pdfax.plot(x[mx], fXprior[mx], '--', label='$X_{%d}^{-}$ prior' % m)
+        pdfax.plot(x[mx], fXinfer[mx], '-.', label='$X_{%d}^{m}$ likelihood' % m)
+        pdfax.plot(x[mx], fXpost[mx], label='$X_{%d}^{+}$ posterior' % m)
+        pdfax.grid(True)
+        pdfax.legend()
 
-    axes[1].legend()
-    #axes[1].text(z, max(fXpost) + 0.1, 'z=%.2f' % z)
-
-    axes[0].set_title('z=%.2f, K=%.2f' % (z, K))
-    
+    if estax is not None:        
+        estax.set_title('z=%.2f, K=%.2f' % (z, K))
+    else:
+        pdfax.set_title('z=%.2f, K=%.2f' % (z, K))        
+        
 
 def kf_demo2():
-    interact(kf_demo2_plot, step=(1, steps), v=(1.0, 4.0, 0.2),
-             sigmaX0=(0.1, 1, 0.1),
+    interact(kf_demo2_plot, show=show_choices,
+             step=(1, steps), v=(1.0, 4.0, 0.2),
+             sigmaX0=(0.1, 2, 0.1),
              sigmaV=(0.1, 2, 0.1),
-             sigmaW=(0.1, 1, 0.1),
+             sigmaW=(0.1, 2, 0.1),
              seed=(1, 100, 1),
              continuous_update=False)
