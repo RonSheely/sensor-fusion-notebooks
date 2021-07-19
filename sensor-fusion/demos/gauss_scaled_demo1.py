3fdc9f72818decf315fad644d4af01868a792cff
Michael Hayes <michael.hayes@canterbury.ac.nz>
2021-07-20 08:06:26 +1200

    Tweak scale steps

--- sensor-fusion/demos/gauss_scaled_demo1.py
+++ sensor-fusion/demos/gauss_scaled_demo1.py
@@ -7,6 +7,9 @@ from .lib.utils import gauss
 
 def gauss_scaled_demo1_plot(muX=0, sigmaX=1, a=1, autoscale=False):
 
+    if a == 0:
+        a = 1e-3
+    
     N = 401
     x = np.linspace(-10, 10, N)
 
@@ -25,7 +28,7 @@ def gauss_scaled_demo1_plot(muX=0, sigmaX=1, a=1, autoscale=False):
 
 def gauss_scaled_demo1():
     interact(gauss_scaled_demo1_plot, muX=(-5, 5), sigmaX=(0.01, 5, 0.01),
-             a = (0.01, 5, 0.01))
+             a = (0.0, 5, 0.1))
     
     
 
