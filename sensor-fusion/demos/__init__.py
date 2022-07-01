9aa6f500be542dc189b582ad9d86eaeb0d5b43bf
Michael Hayes <michael.hayes@canterbury.ac.nz>
2022-07-02 08:41:39 +1200

    Tweak

--- sensor-fusion/demos/__init__.py
+++ sensor-fusion/demos/__init__.py
@@ -28,6 +28,7 @@ from .likelihood_demo1 import *
 from .likelihood_demo2 import *
 from .likelihood_demo3 import *
 from .likelihood_demo4 import *
+from .likelihood_demo5 import *
 from .joint_likelihood_demo1 import *
 from .sampling_demo0 import *
 from .sampling_demo1 import *
